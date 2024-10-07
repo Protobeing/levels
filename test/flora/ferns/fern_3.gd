880df5d2cb553dfd55ba365b66a868c8e1ffb3a2
Protobeing <147507749+Protobeing@users.noreply.github.com>
2024-10-07 12:00:58 -0400

    Add files via upload

---
+++ test/flora/ferns/fern_3.gd
@@ -0,0 +1,14 @@
+extends Area2D
+@onready var anim_2 = $AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_body_entered(_body):
+	anim_2.play("new_animation")
